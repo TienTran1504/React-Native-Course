38b0b51ed9339292fccdb7c66d078814f6bf454b
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-06-07 02:28:15 +0700

    MealsToGo app part 1

---
+++ Course/MealsTogo/src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FlatList, SafeAreaView, Platform, StatusBar } from "react-native";
+import styled from "styled-components/native";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+const isAndroid = Platform.OS === "android";
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 32,
+  },
+})``;
+
+export const RestaurantScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+          { name: 11 },
+          { name: 12 },
+        ]}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 32 }}
+      />
+    </SafeArea>
+  );
+};
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Install react native: npx create-expo-app@latest -e with-router ./
 Install some package: npm i expo-font axios react-native-dotenv
 
+expo init <Project-name>
+
 Course description:
 
 - Learn React, React native foundations
